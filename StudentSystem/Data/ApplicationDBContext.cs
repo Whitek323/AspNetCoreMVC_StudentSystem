c6b837570a73e5af17546a41c4166f42f730d239
ClonePBank <thanarat@japansystem.co.th>
2023-07-20 12:05:27 +0700

    lastCommit

---
+++ StudentSystem/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentSystem.Models;
+
+namespace StudentSystem.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentSystem/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string? Score { get; set; }
+    }
+}
